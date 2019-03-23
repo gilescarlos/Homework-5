64bed9674e5a583b86e51bc45a21762f44ad93c9
gilescarlos32 <46952491+gilescarlos32@users.noreply.github.com>
2019-03-23 14:13:12 -0700

    Add files via upload

---
+++ VirtualDestructor.cpp
@@ -0,0 +1,35 @@
+// virtual destructor example   base pointer problem
+#include<iostream>
+
+class base {
+public:
+    base()
+    {
+        std::cout << "Constructing base \n";
+    }
+    virtual ~base()
+    {
+        std::cout << "Destructing base \n";
+    }
+};
+
+class derived : public base {
+public:
+    derived()
+    {
+        std::cout << "Constructing derived \n";
+    }
+    ~derived()
+    {
+        std::cout << "Destructing derived \n";
+    }
+};
+
+int main(void)
+{
+    derived *d = new derived();
+    base *b = d;
+    delete b;
+    getchar();
+    return 0;
+}
